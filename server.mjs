e89f57d5ab56a380e644fa0c7295041bea0c66ee
DESKTOP-1QJ3QEI <syednadeemali221@gmail.com>
2022-10-28 17:23:08 +0500

    some work

--- server.mjs
+++ server.mjs
@@ -2,6 +2,14 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT || 3000
 
+let todos = []
+
+app.post('/todo' , (req , res)  => {
+  console.log('main page')
+  res.send('you ask for /water/pizza and /coffee')
+
+  })
+
 app.get('/name', (req, res) => {
   res.send('Nadeem Hakeem')
 })
@@ -9,6 +17,11 @@ app.get('/name', (req, res) => {
 app.get('/water', (req, res) => {
     res.send('here is some water for you')
   })
+
+app.post('/water', (req, res) => {
+    res.send('some one offering a water')
+  })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
